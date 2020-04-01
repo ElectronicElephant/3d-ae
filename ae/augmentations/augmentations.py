Fix the problem where the norm is TOO big Fix the problem where the DAE tends to learn the average

ae/augmentations/augmentations.py
@@ -38,8 +38,8 @@ class AddRandomNoise(object):
         self.sigma = sigma
 
     def __call__(self, img):
-        gauss = np.random.normal(self.mean, self.sigma, img.shape) * (255 * self.sigma * 2)
-        return (img + gauss).astype(np.uint8)
+        gauss = np.random.normal(self.mean, self.sigma, img.shape) * (self.sigma * 2)
+        return (img + gauss).clip(0, 1)
 
 
 class ToTensor(object):

ae/loss/loss.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 
 
 def dae_loss(input, target, weight=None, reduction='mean'):
-    loss = F.binary_cross_entropy(input, target, reduction=reduction)
+    loss = F.binary_cross_entropy(input, target, reduction=reduction, weight=weight)
     return loss
 
 